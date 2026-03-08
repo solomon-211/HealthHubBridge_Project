d97e6e0c5b24eeef6f27f3cf774c93a77a4a88b3
ToluwaniOladeji <t.oladeji@alustudent.com>
2026-03-08 15:22:11 +0200

    created config file and added the class that stores all the configuration settings for my flask backend

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'healthbridge-dev-secret-key')
+
+    DB_HOST     = os.environ.get('DB_HOST',     'localhost')
+    DB_PORT     = int(os.environ.get('DB_PORT', 3306))
+    DB_USER     = os.environ.get('DB_USER',     'root')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
+    DB_NAME     = os.environ.get('DB_NAME',     'healthbridge_db')
+
+    
+    SESSION_LIFETIME = int(os.environ.get('SESSION_LIFETIME', 3600)) # 1 hour default. After this, users must log in again. Reduces risk of stolen sessions.
+
+    CACHE_TTL = int(os.environ.get('CACHE_TTL', 30))
+
+
