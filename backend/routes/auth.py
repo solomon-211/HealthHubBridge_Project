7fc14d02a7abdb3f988b6425c1bb616e424c2a54
ToluwaniOladeji <t.oladeji@alustudent.com>
2026-03-10 15:55:05 +0200

    Added the dependencies needed, initialized the auth blueprint and added the function to encode passwords to SHA-256

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,1 +1,13 @@
-#
+from flask import Blueprint, request, jsonify, session
+from config import get_db_connection, Config
+import hashlib
+
+
+# initializes the Flask Blueprint for the authentication route.
+auth_bp = Blueprint('auth', __name__)
+
+# to encode passwords from plain text to a SHA-256 hash.
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
